e2195e4003e5b01634f25104a4010e36ef5cd5fa
Ramazan Azimli <80627823+antisyst@users.noreply.github.com>
2023-03-04 10:07:03 +0400

    Update skills-showcase.component.tsx

--- src/components/skills-showcase/skills-showcase.component.tsx
+++ src/components/skills-showcase/skills-showcase.component.tsx
@@ -24,31 +24,10 @@ function SkillsShowcase() {
                     <p><i className="fa-solid fa-circle-notch"></i>Demonstrated experience using ReactJS tools like <span>Redux</span>, <span>Router</span>, <span>Hooks</span> and <span>GraphQL</span>.</p>
                 </li>
                 <li>
-                    <p><i className="fa-solid fa-circle-notch"></i>Strong skills with <span>CSS</span> frameworks such as <span>Bootstrap</span>, <span>TailwindCSS</span>, <span>UIkit</span>, <span>pureCSS</span>, <span>Skeleton</span> and <span>daisyUI</span>.</p>
+                    <p><i className="fa-solid fa-circle-notch"></i>Strong skills with <span>JavaScript</span> and <span>jQuery</span>, knowledge of code versioning using <span>Git</span> Control System.</p>
                 </li>
                 <li>
-                    <p><i className="fa-solid fa-circle-notch"></i>Strong skills with <span>JavaScript</span>, <span>AJAX</span> and <span>jQuery</span>, knowledge of code versioning using <span>Git</span> Control System.</p>
-                </li>
-                <li>
-                    <p><i className="fa-solid fa-circle-notch"></i>Proficient understanding of cross-browser compatibility issues and ways to work around them.</p>
-                </li>
-                <li>
-                    <p><i className="fa-solid fa-circle-notch"></i>Demonstrate an understanding of the needs of growing businesses, including emerging digital and web development trends.</p>
-                </li>
-                <li>
-                    <p><i className="fa-solid fa-circle-notch"></i>Strong skills and knowledge <span>ReactJS</span> component library such as <span>MaterialUI</span>,  <span>NextUI</span>, <span>Prime React</span>, <span>Motion Framer</span>, <span>React Reveal</span>, <span>Blueprint UI</span>,  <span>Ant Design</span>, <span>Onsen UI</span>, <span>Grommet</span>,<span>Core UI</span>, <span>Semantic UI</span>, <span>React Icons</span> <span>React Bootstrap</span>, <span>ChakraUI</span> and etc.</p>
-                </li>
-                <li>
-                    <p><i className="fa-solid fa-circle-notch"></i>Contributing in building and maintaining internal as well as customer facing frontend applications.</p>
-                </li>
-                <li>
-                    <p><i className="fa-solid fa-circle-notch"></i>Testing to identify bugs and technical issues before and after deploying.</p>
-                </li>
-                <li>
-                    <p><i className="fa-solid fa-circle-notch"></i>Promote best practices in <span>UI/UX</span> and components build scalable websites utilizing open-source tools.</p>
-                </li>
-                <li>
-                    <p><i className="fa-solid fa-circle-notch"></i>Proficiency with <span>PWA</span> ( Progressive Web Application ) .</p>
+                    <p><i className="fa-solid fa-circle-notch"></i>Strong skills and knowledge <span>ReactJS</span> component library such as <span>MaterialUI</span>,  <span>NextUI</span>, <span>Prime React</span>, <span>React Reveal</span>,  <span>Ant Design</span>,  <span>Grommet</span>, <span>React Icons</span>  and etc.</p>
                 </li>
             </div>
 
